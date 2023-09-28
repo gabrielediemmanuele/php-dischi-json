12375f7b782e9a238c0664282cd266f91fd7af8a
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-28 15:40:48 +0200

    feat: add all the files

---
+++ front-end/main.js
@@ -0,0 +1,16 @@
+//* Instanza Vue JS
+
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      message: "Hello Tutors!",
+    };
+  },
+
+  /* methods: {
+    greetings: function () {
+        return 'ciao';
+    }
+  } */
+}).mount("#app");
