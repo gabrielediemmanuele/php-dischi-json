0808aa38f51fb0a0d2017612f25ae38e6dd514a7
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-28 18:26:59 +0200

    feat: at click show album info in  log - noproxy

--- front-end/main.js
+++ front-end/main.js
@@ -5,15 +5,16 @@ createApp({
   data() {
     return {
       albums: [],
+      selectedAlbum: "",
     };
   },
-
-  /* methods: {
-    greetings: function () {
-        return 'ciao';
-    }
-  } */
-
+  methods: {
+    getCardInfo(index) {
+      this.albums[index];
+      console.log(JSON.parse(JSON.stringify(this.albums[index])));
+      /* console.log(this.albums[index]); */
+    },
+  },
   /*mounted di axios */
   mounted() {
     axios
@@ -27,3 +28,12 @@ createApp({
       });
   },
 }).mount("#app");
+
+/* al click sull'album, stampo in console i dati 
+contenuti nel suo indice.. come procedo?
+clicco sull immagine e scateno la funzione(metodo)
+cosa deve fare la funzione? in base all indice cliccato, mi mostra il suo contenuto..
+albums[index]
+
+
+*/
